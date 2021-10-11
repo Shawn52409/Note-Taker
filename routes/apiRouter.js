ceb9129aa894cab96c8cede490e770525b3658ad
Shawn Miller <shawnsmm@gmail.com>
2021-10-10 23:07:57 -0400

    wip fixed some issues with server.js and both routers

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,9 +1,48 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
 const fs = require('fs');
+var notes = require('../db/db.json');
+
+// Get notes
+router.get('/notes', (req, res) => {
+    return res.json(notes);
+});
+
+// Create new note
+router.post('/notes', (req, res) => {
+    let newNote = req.body;
+    // console.log(req.body);
+    
+    // give every note a unique id that will always be different
+    newNote["id"] = Date.now();            
+    
+    notes.push(newNote);
+
+    writeNotes();
+
+    return res.status(200).end();
+});
+
+// delete note at specified id
+router.delete('/notes/:id', (req, res) => {
+    res.send('DELETE request at /api/notes/:id')
+    console.log(req.params);
+    for (i=0; i<notes.length; i++){
+        console.log(notes[i].id);
+    }
+    notes.splice(req.params, 1);
+    writeNotes();
+});
 
 
-
+const writeNotes = () => {
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+            console.log('Error')
+            return console.log(err);
+        };
+        console.log('Success!!!');
+    });
+};
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 // Require Dependencies
-const fs = require("fs");
-const path = require('path');
+const fs = require('fs');
 const express = require("express");
 
 // Create router routes
 const htmlRouter = require('./routes/htmlRouter');
 const apiRouter = require('./routes/apiRouter');
-
-// Variable used for all notes
-const allNotes = require('./db/db.json');
 
 // Port setting
 const PORT = process.env.PORT || 3001;
@@ -21,11 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // set static folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Router
-app.use('/', htmlRouter);
 app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
 
 
